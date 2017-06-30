10c0232ff70c25e8b169529ff1952a00c2755700
Chandan Kumar Baba <cbaba20@gmail.com>
2017-07-01 01:15:55 +0530

    code creation

---
+++ MultiDimArray/MultiDimArray/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiDimArrayDemo
+{
+    class Program
+    {
+        public void Run()
+        {
+            const int rows = 4;
+            const int cols = 3;
+
+            //declare 4*3 integer array
+
+            int[,] rectangularArray = new int[rows, cols];
+
+            //populate the array
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    rectangularArray[i, j] = i + j;
+                }
+            }
+
+            //display the populated array
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    Console.WriteLine("rectangularArray[{0},{1}]={2}", i, j, rectangularArray[i, j]);
+                }
+            }
+
+        }
+        [STAThread]
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            p.Run();
+            Console.ReadLine();
+        }
+    }
+}
